713fc5d87e1508378e05062d968cfa37ce547830
markadurst <markadurst@gmail.com>
2025-09-13 09:06:19 -0600

    Fix GitHub Pages routing and React Router basename

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { CartProvider } from './context/CartContext'
 function App() {
   return (
     <CartProvider>
-      <Router>
+      <Router basename="/annas-sweet-crafts">
         <div className="App">
           <Header />
           <main>
